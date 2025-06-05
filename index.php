1615deb111beb276af77529d542b8fc8141d8a42
b4h3ru <Bahiruwalelign@gmail.com>
2025-06-05 10:52:37 -0900

    api rout updated

--- index.php
+++ index.php
@@ -6,30 +6,30 @@ $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // echo $uri; ///task-manger-api/
-if ($uri === '/task-manger-api/' && $method === 'GET') {
+if ($uri === '/task-manager-api/' && $method === 'GET') {
     echo json_encode(["my task manager api is running"]);
     exit;
 }
 
 // $uriParts = explode('/task-manger-api/', trim($uri, '/'));
 
-if ($uri === '/task-manger-api/api/tasks' && $method === 'GET') {
+if ($uri === '/task-manager-api/api/tasks' && $method === 'GET') {
     require 'tasks/get.php';
     exit;
 }
 
 //user to add new task 
-if ($uri === '/task-manger-api/api/tasks' && $method === 'POST') {
+if ($uri === '/task-manager-api/api/tasks' && $method === 'POST') {
     require 'tasks/post.php';
     exit;
 }
 //
-if ($uri === '/task-manger-api/api/tasks' && $method === 'PUT') {
+if ($uri === '/task-manager-api/api/tasks' && $method === 'PUT') {
     require 'tasks/put.php';
     exit;
 }
 
-if ($uri === '/task-manger-api/api/tasks' && $method === 'DELETE') {
+if ($uri === '/task-manager-api/api/tasks' && $method === 'DELETE') {
     require 'tasks/delete.php';
     exit;
 }
